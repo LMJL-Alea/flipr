95b37855e80bfe92b6d6f954e766d00230137fcc
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-03-04 00:22:14 +0100

    Added package documentation.

---
+++ R/flipr-package.R
@@ -0,0 +1,14 @@
+#' flipr: Flexible inference via permutations in R
+#'
+#' The flipr package provides a flexible permutation framework for making
+#' inference such as point estimation, confidence intervals or hypothesis
+#' testing, on any kind of data, be it univariate, multivariate, or more complex
+#' such as network-valued data, topological data, functional data or
+#' density-valued data.
+#'
+#' @docType package
+#' @name flipr
+#' @import ggplot2
+#' @importFrom tibble tibble
+#' @importFrom rlang .data
+NULL
