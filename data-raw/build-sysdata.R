f38b057e17f15be092a2582a493545de97c7c457
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-08-30 22:48:00 +0200

    Updated vignettes to match new API.

--- data-raw/build-sysdata.R
+++ data-raw/build-sysdata.R
@@ -1,12 +1,14 @@
-dfa <- readRDS("data-raw/dfa.rds")
-dfb <- readRDS("data-raw/dfb.rds")
-dfc <- readRDS("data-raw/dfc.rds")
+alpha_estimates <- readRDS("data-raw/alpha.rds")
+pfa <- readRDS("data-raw/pfa.rds")
+pfb <- readRDS("data-raw/pfb.rds")
+pfc <- readRDS("data-raw/pfc.rds")
 df_mean <- readRDS("data-raw/df_mean.rds")
 df_sd <- readRDS("data-raw/df_sd.rds")
 df_fisher <- readRDS("data-raw/df_fisher.rds")
 df_tippett <- readRDS("data-raw/df_tippett.rds")
 usethis::use_data(
-  dfa, dfb, dfc,
+  alpha_estimates,
+  pfa, pfb, pfc,
   df_mean, df_sd,
   df_fisher, df_tippett,
   overwrite = TRUE,
